3b88c8ee801455a73de32189a63574ad20217a5d
kinjal13077 <kinjalkharwa2007@gmail.com>
2025-11-21 10:31:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:8080",
-  "https://ecommerce-frontend-fawn-ten-54.vercel.app",
+  "https://ecommerece-frontend-ten.vercel.app",
 ];
 
 app.use(
